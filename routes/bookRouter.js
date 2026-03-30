d69b9388b7c76499355232296e2114f8de8f7e53
Rehab <rehabmohd03@gmail.com>
2026-03-30 15:53:27 +0300

    add update function

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -7,6 +7,7 @@ router.post("/create", bookController.createBook)
 router.get("/create", (req, res) => {
   res.render("../views/createBook.ejs")
 })
+router.put("/:id", bookController.updateBookById)
 router.delete("/:id", bookController.deleteBookById)
 
 module.exports = router
