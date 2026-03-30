46ac96c58c1adc8bd5f2872303030308ed4d4ce5
masooma99 <masooe99@gmail.com>
2026-03-30 09:55:47 +0300

    update sign in

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,7 +2,48 @@ const Book = require("../models/Book")
 const LibraryBook = require("../models/LibraryBook")
 const User = require("../models/User")
 
-const createBook = async (req, res) => {}
+const createBook = async (req, res) => {
+  try {
+    const bookInDB = await Book.exists({ title: req.body.title })
+
+    let tempBook
+
+    if (bookInDB) {
+      const bookId = await Book.findOne({ title: req.body.title })
+      tempBook = bookId
+      // res.send("This book is already added in the DB")
+    } else {
+      const newBook = await Book.create({
+        title: req.body.title,
+        description: req.body.description,
+        genre: req.body.genre,
+        numOfPages: req.body.numOfPages,
+        author: req.body.author,
+      })
+      tempBook = newBook
+    }
+
+    if (!(await LibraryBook.exists({ book: tempBook._id }))) {
+      await LibraryBook.create({
+        numOfCopies: req.body.numOfCopies,
+        book: tempBook,
+        user: req.session.user,
+      })
+    }
+    const userBooks = await LibraryBook.find({ user: req.session.user })
+
+    const libraryDetails = {
+      user: req.session.user,
+      userBooks: userBooks,
+    }
+    console.log(userBooks)
+    req.session.save(() => {
+      return res.render("../views/userPage.ejs", { libraryDetails })
+    })
+  } catch (error) {
+    res.send(`error: ${error}`)
+  }
+}
 
 const findByTitle = async (req, res) => {
   try {
