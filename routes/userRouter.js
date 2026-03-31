8dc240d97d5864ced378a03e4bd2359274673f3e
batool <batooola.97@gmail.com>
2026-03-31 09:48:05 +0300

    genre

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const router = express.Router()
+const Book = require("../models/Book")
 
 const userController = require("../controllers/userController")
 
 router.get("/", userController.testing)
-
+router.get("/home", (req, res) => {
+  res.render("../views/home.ejs", { Book })
+})
 module.exports = router
