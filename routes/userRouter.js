ed2920a3acb2424adb33293e3f3521ef87bf3573
masooma99 <masooe99@gmail.com>
2026-04-01 17:41:56 +0300

    ...

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,11 +3,7 @@ const router = express.Router()
 
 const userController = require("../controllers/userController")
 
-<<<<<<< HEAD
-router.get("/books/:id", userController.getBookById)
-=======
 router.get("/:id", userController.getBookById)
->>>>>>> 7b2af86d22964e02eddbb381e7ca0ac3ed62ba6f
 router.get("/", userController.getUserById)
 
 module.exports = router
